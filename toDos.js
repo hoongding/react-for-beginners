9d803a867a5db513493c48f46b652eead2126400
JangHoonSeok <orirori512@naver.com>
2022-08-07 15:16:51 +0900

    toDos.js

---
+++ toDos.js
@@ -0,0 +1,43 @@
+import { toBeEmptyDOMElement } from "@testing-library/jest-dom/dist/matchers";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  const deleteBtn = (index) =>
+    setToDos((curToDos) => curToDos.filter((__, curIdx) => curIdx !== index));
+  return (
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteBtn(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
